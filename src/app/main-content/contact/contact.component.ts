da6231a8c856b3639c87ddc475d5e20988f00410
@r-s-20 <robin_s_2020@mailbox.org>
2024-07-29 16:17:29 +0200

    added checkbox icons for contact section

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  policyChecked = false;
 
+  togglePolicyCheck() {
+    let uncheckedIcon = document.getElementById('unchecked');
+    let checkedIcon = document.getElementById('checked');
+    if (this.policyChecked) {
+      this.policyChecked = false;
+      checkedIcon?.classList.add("d-none");
+      uncheckedIcon?.classList.remove("d-none");
+    } else {
+      this.policyChecked = true;
+      checkedIcon?.classList.remove("d-none");
+      uncheckedIcon?.classList.add("d-none");
+    }
+  }
 }
