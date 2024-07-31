e7742592fdbdcff746a8e27ecedc1c81a4e8f83b
@r-s-20 <robin_s_2020@mailbox.org>
2024-07-31 14:49:26 +0200

    adjusted contact section to new layout

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -5,8 +5,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      name: 'Join',
+      skills: 'HTML, CSS, Javascript, Supabase',
+      url: '',
+    },
+    {
+      name: 'Sharky',
+      skills: 'OOP, HTML, CSS, Javascript',
+      url: '',
+    },
+    {
+      name: 'Pokédex',
+      skills: 'Rest-API, HTML, CSS, Javascript',
+      url: '',
+    },
+  ];
 }
