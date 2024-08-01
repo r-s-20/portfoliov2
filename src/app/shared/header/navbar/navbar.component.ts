9ad5d7dec57f8c024630be97a045246cf997709f
@r-s-20 <robin_s_2020@mailbox.org>
2024-08-01 16:19:43 +0200

    added lang button

--- src/app/shared/header/navbar/navbar.component.ts
+++ src/app/shared/header/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  lang: 'EN' | 'DE' = 'EN';
 
+  toggleLang() {
+    if (this.lang == 'EN') {
+      this.lang = 'DE';
+    } else this.lang = 'EN';
+    console.log(this.lang);
+  }
 }
