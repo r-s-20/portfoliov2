e534b25063608ffa3cd443c049609929a2aaaae6
unknown <r_s_2020@mailbox.org>
2024-08-02 10:25:56 +0200

    added control for heroAnimation

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../shared/header/header.component";
+import { HeaderComponent } from '../../shared/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  animateHero: boolean = false;
 }
