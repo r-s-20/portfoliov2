87d0eec4b28521ba2e80fa67227d7db63d648f32
@r-s-20 <robin_s_2020@mailbox.org>
2024-08-07 15:10:16 +0200

    improvements to responsive breakpoint 2

--- src/app/shared/header/navbar-mobile/navbar-mobile.component.ts
+++ src/app/shared/header/navbar-mobile/navbar-mobile.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-mobile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar-mobile.component.html',
-  styleUrl: './navbar-mobile.component.scss'
+  styleUrl: './navbar-mobile.component.scss',
 })
 export class NavbarMobileComponent {
+  showMenu: boolean = false;
+  openMenu() {
+    this.showMenu = true;
+  }
+  closeMenu() {
+    this.showMenu = false;
+  }
+  toggleMenu() {
+    if (this.showMenu) {
+      this.showMenu = false;
+    } else {
+      this.showMenu = true;
+    }
+  }
+  lang: 'EN' | 'DE' = 'EN';
+  heroAnimation: boolean = false;
 
+  toggleLang() {
+    if (this.lang == 'EN') {
+      this.lang = 'DE';
+    } else this.lang = 'EN';
+    console.log(this.lang);
+  }
+
+  toggleAnimation() {
+
+  }
 }
